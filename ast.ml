213185b3cec0417fcd6d78e31b8d56f34dad8458
Adith <ar729@cornell.edu>
2020-10-31 02:13:42 -0400

    initial commit

---
+++ ast.ml
@@ -0,0 +1,6 @@
+(**
+  This is the Abstract Search Tree that represents the
+  SimpleTeX language
+ *)
+
+ exception SyntaxError
---
+++ authors.mli
@@ -0,0 +1,10 @@
+(**
+  metadata about this submission
+  @author Adith Ramachandran(ar729), Atul Ganju(ag2222)
+*)
+
+(**
+  [hours_worked] is a list of the number of hours each team member
+  worked on this assignment
+*)
+val hours_worked : int list
